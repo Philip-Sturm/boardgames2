1cb0ae601313aa276fc10a35144783f73ee5c6a9
Reiner Huechting <reinerh-dev@pm.me>
2026-02-24 08:25:43 +0100

    Remove hints.

--- queens/game.go
+++ queens/game.go
@@ -8,8 +8,6 @@ type Game struct {
 
 // NewGame erstellt ein neues Spiel mit einem leeren 8x8-Spielfeld.
 func NewGame() *Game {
-	// Hinweis:
-	// Erstellen Sie ein neues Board mit 8 Zeilen und 8 Spalten.
 	// TODO
 	return &Game{}
 }
@@ -17,31 +15,22 @@ func NewGame() *Game {
 // Get liefert den Inhalt der Zelle an der angegebenen Position zurück.
 // Liefert einen leeren String, falls die Position außerhalb des Spielfelds liegt.
 func (g *Game) Get(row, col int) string {
-	// Hinweis:
-	// Verwenden Sie die Get-Methode des Boards, um den Inhalt der Zelle zu erhalten.
 	// TODO
 	return ""
 }
 
 // Set setzt eine Dame in der Zelle an der angegebenen Position.
 func (g *Game) Set(row, col int) {
-	// Hinweis:
-	// Entscheiden Sie sich für ein Symbol, um eine Dame darzustellen.
-	// Setzen Sie dieses Symbol mit der Set-Methode des Boards.
 	// TODO
 }
 
 // Unset entfernt eine Dame aus der Zelle an der angegebenen Position.
 func (g *Game) Unset(row, col int) {
-	// Hinweis:
-	// Entfernen Sie die Dame, indem Sie die Zelle mit einem leeren String überschreiben.
 	// TODO
 }
 
 // String gibt eine textuelle Darstellung des Spiels zurück.
 func (g Game) String() string {
-	// Hinweis:
-	// Verwenden Sie die String-Methode des Boards.
 	// TODO
 	return ""
 }
--- queens/logic.go
+++ queens/logic.go
@@ -3,23 +3,12 @@ package queens
 // MoveAllowed prüft, ob ein Zug an der angegebenen Position erlaubt ist.
 // Ein Zug ist erlaubt, wenn die Position innerhalb des Spielfelds liegt und die Zelle an dieser Position leer ist.
 func (g *Game) MoveAllowed(row, col int) bool {
-	// Hinweis:
-	// Benutzen Sie die Methoden von `Board` um zu überprüfen, ob die Zeilen, Spalten und Diagonalen frei sind.
-	// Bestimmen Sie vorher (für die Diagonalen) zum gegebenen (row/col)-Paar, in welcher Spalte die entsprechenden
-	// Diagonalen beginnen.
-
 	// TODO
 	return false
 }
 
 // Solve löst das Spiel.
 func (g *Game) Solve(row int) bool {
-	// Hinweis:
-	// Gehen Sie rekursiv vor.
-	// Als Abbruchbedingung können Sie verwenden, dass row den Wert 8 verwendet hat.
-	// Gehen Sie dann in der aktuellen Zeile alle Felder durch, setzen Sie eine Dame (falls erlaubt),
-	// und rufen Sie die Lösungsfunktion mit der nächsten Zeile auf.
-
 	// TODO
 	return false
 }
