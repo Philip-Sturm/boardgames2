e246f1fdf6a4bd99093637b941b36e689fbdc5e4
Reiner Huechting <reinerh-dev@pm.me>
2026-02-24 08:24:57 +0100

    Remove solutions.

--- queens/game.go
+++ queens/game.go
@@ -10,9 +10,8 @@ type Game struct {
 func NewGame() *Game {
 	// Hinweis:
 	// Erstellen Sie ein neues Board mit 8 Zeilen und 8 Spalten.
-	return &Game{
-		board: board.New(8, 8),
-	}
+	// TODO
+	return &Game{}
 }
 
 // Get liefert den Inhalt der Zelle an der angegebenen Position zurück.
@@ -20,7 +19,8 @@ func NewGame() *Game {
 func (g *Game) Get(row, col int) string {
 	// Hinweis:
 	// Verwenden Sie die Get-Methode des Boards, um den Inhalt der Zelle zu erhalten.
-	return g.board.Get(row, col)
+	// TODO
+	return ""
 }
 
 // Set setzt eine Dame in der Zelle an der angegebenen Position.
@@ -28,19 +28,20 @@ func (g *Game) Set(row, col int) {
 	// Hinweis:
 	// Entscheiden Sie sich für ein Symbol, um eine Dame darzustellen.
 	// Setzen Sie dieses Symbol mit der Set-Methode des Boards.
-	g.board.Set(row, col, "*")
+	// TODO
 }
 
 // Unset entfernt eine Dame aus der Zelle an der angegebenen Position.
 func (g *Game) Unset(row, col int) {
 	// Hinweis:
 	// Entfernen Sie die Dame, indem Sie die Zelle mit einem leeren String überschreiben.
-	g.board.Set(row, col, " ")
+	// TODO
 }
 
 // String gibt eine textuelle Darstellung des Spiels zurück.
 func (g Game) String() string {
 	// Hinweis:
 	// Verwenden Sie die String-Methode des Boards.
-	return g.board.String()
+	// TODO
+	return ""
 }
--- queens/logic.go
+++ queens/logic.go
@@ -8,14 +8,8 @@ func (g *Game) MoveAllowed(row, col int) bool {
 	// Bestimmen Sie vorher (für die Diagonalen) zum gegebenen (row/col)-Paar, in welcher Spalte die entsprechenden
 	// Diagonalen beginnen.
 
-	r, cl, cr := row, col, col
-	for ; r > 0; r, cl, cr = r-1, cl-1, cr+1 {
-	}
-
-	return !g.board.RowContains(row, "*") &&
-		!g.board.ColContains(col, "*") &&
-		!g.board.DiagDownRightContains(cl, "*") &&
-		!g.board.DiagDownLeftContains(cr, "*")
+	// TODO
+	return false
 }
 
 // Solve löst das Spiel.
@@ -25,18 +19,7 @@ func (g *Game) Solve(row int) bool {
 	// Als Abbruchbedingung können Sie verwenden, dass row den Wert 8 verwendet hat.
 	// Gehen Sie dann in der aktuellen Zeile alle Felder durch, setzen Sie eine Dame (falls erlaubt),
 	// und rufen Sie die Lösungsfunktion mit der nächsten Zeile auf.
-	if row == 8 {
-		return true
-	}
 
-	for col := 0; col < 8; col++ {
-		if g.MoveAllowed(row, col) {
-			g.Set(row, col)
-			if g.Solve(row + 1) {
-				return true
-			}
-			g.Unset(row, col)
-		}
-	}
+	// TODO
 	return false
 }
