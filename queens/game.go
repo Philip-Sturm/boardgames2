bd5bf04eb06106c1bdca9e6c6af7d6f1f2da7242
Reiner Huechting <reinerh-dev@pm.me>
2026-02-24 08:22:42 +0100

    Add hints.

--- queens/game.go
+++ queens/game.go
@@ -8,6 +8,8 @@ type Game struct {
 
 // NewGame erstellt ein neues Spiel mit einem leeren 8x8-Spielfeld.
 func NewGame() *Game {
+	// Hinweis:
+	// Erstellen Sie ein neues Board mit 8 Zeilen und 8 Spalten.
 	return &Game{
 		board: board.New(8, 8),
 	}
@@ -21,15 +23,22 @@ func (g *Game) Get(row, col int) string {
 
 // Set setzt eine Dame in der Zelle an der angegebenen Position.
 func (g *Game) Set(row, col int) {
+	// Hinweis:
+	// Entscheiden Sie sich für ein Symbol, um eine Dame darzustellen.
+	// Setzen Sie dieses Symbol mit der Set-Methode des Boards.
 	g.board.Set(row, col, "*")
 }
 
 // Unset entfernt eine Dame aus der Zelle an der angegebenen Position.
 func (g *Game) Unset(row, col int) {
+	// Hinweis:
+	// Entfernen Sie die Dame, indem Sie die Zelle mit einem leeren String überschreiben.
 	g.board.Set(row, col, " ")
 }
 
 // String gibt eine textuelle Darstellung des Spiels zurück.
 func (g Game) String() string {
+	// Hinweis:
+	// Verwenden Sie die String-Methode des Boards.
 	return g.board.String()
 }
