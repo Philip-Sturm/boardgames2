99992a7d5e3bb363a5b0ab9b8831439c5f27ab3d
Philip <philip_sturm@outlook.de>
2026-02-26 10:47:50 +0100

    brüchige ansätze

--- queens/game.go
+++ queens/game.go
@@ -1,6 +1,9 @@
 package queens
 
-import "boardgames/board"
+import (
+	"boardgames/board"
+	"fmt"
+)
 
 type Game struct {
 	board *board.Board
@@ -8,29 +11,36 @@ type Game struct {
 
 // NewGame erstellt ein neues Spiel mit einem leeren 8x8-Spielfeld.
 func NewGame() *Game {
-	// TODO
-	return &Game{}
+	return &Game{board: board.New(8, 8)}
 }
 
 // Get liefert den Inhalt der Zelle an der angegebenen Position zurück.
 // Liefert einen leeren String, falls die Position außerhalb des Spielfelds liegt.
 func (g *Game) Get(row, col int) string {
-	// TODO
-	return ""
+	if row < 0 || row > 8 || col < 0 || col > 8 {
+		return ""
+	}
+	return g.board.Get(row, col)
 }
 
 // Set setzt eine Dame in der Zelle an der angegebenen Position.
 func (g *Game) Set(row, col int) {
-	// TODO
+	g.board.Set(row, col, "♛")
 }
 
 // Unset entfernt eine Dame aus der Zelle an der angegebenen Position.
 func (g *Game) Unset(row, col int) {
-	// TODO
+	g.board.Set(row, col, "")
 }
 
 // String gibt eine textuelle Darstellung des Spiels zurück.
 func (g Game) String() string {
-	// TODO
-	return ""
+	var res string
+	for i := 0; i <= 7; i++ {
+		for j := 0; j <= 8; j++ {
+			res = res + fmt.Sprint(g.board.Get(i, j))
+		}
+		res = res + fmt.Sprintln("")
+	}
+	return res
 }
--- queens/logic.go
+++ queens/logic.go
@@ -3,7 +3,9 @@ package queens
 // MoveAllowed prüft, ob ein Zug an der angegebenen Position erlaubt ist.
 // Ein Zug ist erlaubt, wenn die Position innerhalb des Spielfelds liegt und die Zelle an dieser Position leer ist.
 func (g *Game) MoveAllowed(row, col int) bool {
-	// TODO
+	if g.board.Get(row, col) == "" {
+		return true
+	}
 	return false
 }
 
